7d51cfb5b2d6f95a12e31fd49aa9b4e842912a9a
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-08-19 17:09:02 +0200

    Added initial files based off of JavaSrc2Cpg

---
+++ build.sbt
@@ -0,0 +1,27 @@
+name := "jimple2cpg"
+organization := "io.joern"
+publish / skip := true
+
+version := "0.1"
+scalaVersion := "2.13.4"
+
+val cpgVersion       = "1.3.281"
+val scalatestVersion = "3.1.1"
+
+fork := true
+resolvers += Resolver.mavenLocal
+trapExit := false
+
+libraryDependencies ++= Seq(
+  "io.shiftleft"  %% "codepropertygraph"        % cpgVersion,
+  "io.shiftleft"  %% "semanticcpg"              % cpgVersion,
+  "io.shiftleft"  %% "dataflowengineoss"        % cpgVersion,
+  "io.shiftleft"  %% "semanticcpg-tests"        % cpgVersion       % Test classifier "tests",
+  "org.soot-oss" % "soot" % "4.2.1",
+  "org.scalatest" %% "scalatest"                % scalatestVersion % Test
+)
+
+enablePlugins(JavaAppPackaging)
+
+Global / onChangedBuildSource := ReloadOnSourceChanges
+       	 
---
+++ src/main/scala/io/joern/jimple2cpg/passes/AstCreationPass.scala
@@ -0,0 +1,26 @@
+package io.joern.jimple2cpg.passes
+
+
+import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.passes.{DiffGraph, IntervalKeyPool, ParallelCpgPass}
+import org.slf4j.LoggerFactory
+
+import java.util.concurrent.ConcurrentHashMap
+
+case class Global(
+    usedTypes: ConcurrentHashMap[String, Boolean] = new ConcurrentHashMap[String, Boolean]()
+)
+
+class AstCreationPass(codeDir: String, filenames: List[String], cpg: Cpg, keyPool: IntervalKeyPool)
+    extends ParallelCpgPass[String](cpg, keyPools = Some(keyPool.split(filenames.size))) {
+
+  val global: Global = Global()
+  private val logger = LoggerFactory.getLogger(classOf[AstCreationPass])
+
+  override def partIterator: Iterator[String] = filenames.iterator
+
+  override def runOnPart(filename: String): Iterator[DiffGraph] = {
+    Iterator()
+  }
+
+}
